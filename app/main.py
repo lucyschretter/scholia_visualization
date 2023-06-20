9ff99d1a6329c5c6d404b40155273758df6bf5cc
Berfîn Kilic <92948406+kberfin@users.noreply.github.com>
2023-06-20 21:02:28 +0200

    Added mental depression as disease

--- app/main.py
+++ app/main.py
@@ -10,6 +10,8 @@ from util import plot_histogram
 from util import pie_plot_symptoms
 from util import publications_per_year_schizophrenia
 from util import clinical_trials_per_year
+from util import depression_publications_per_year
+from util import depression_co_occuring_topics
 from util import clinical_trials_per_year_bulimia
 
 from dash import dcc
@@ -28,6 +30,7 @@ app.layout = html.Div(children=[
             {'label': 'Bipolar Disorder', 'value': 'Q131755'},
             {'label': 'Migraine', 'value': 'Q133823'},
             {'label': 'Schizophrenia', 'value': 'Q41112'},
+            {'label': 'Mental Depression', 'value': 'Q4340209'},
             {'label': 'Bulimia Nervosa', 'value' : 'Q64513386'}
         ],
         value='Select Diasese'
@@ -61,6 +64,11 @@ def update_graph(disease):
     elif disease == 'Q64513386':
         return [
             dcc.Graph(id='clinical-trials-per-year-bulimia', figure=clinical_trials_per_year_bulimia())
+        ]
+    elif disease == 'Q4340209':
+        return [
+            dcc.Graph(id='publications-per-year-mental-depression', figure=depression_publications_per_year()),
+            dcc.Graph(id='co-occuring-topics', figure=depression_co_occuring_topics())
         ]
     else:
         return html.Div('Select disease')
